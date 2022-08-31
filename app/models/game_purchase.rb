8bf087fb7ff7d13b7e76dbb88080b2a137a845b8
RussellCarey <62222824+RussellCarey@users.noreply.github.com>
2022-08-31 17:51:13 +0900

    Created helper for SQL query

--- app/models/game_purchase.rb
+++ app/models/game_purchase.rb
@@ -11,32 +11,34 @@ class GamePurchase < ApplicationRecord
     #! ADD GENRE, PLATFORM etc onto the purchase...
     # Get total value of all games purchased / by gernre / by platform etc.
     def self.get_total_sales_amount
-        sql = "SELECT game_id, SUM(total) as total FROM game_purchases"
-        record = ActiveRecord::Base.connection.execute(sql)
-        puts record[0].inspect
+        data = run_sql("SELECT game_id, SUM(total) as total FROM game_purchases")
+        puts data.inspect
     end
 
     def self.get_total_sales
-        sql = "SELECT COUNT(total) as total_sales FROM game_purchases"
-        record = ActiveRecord::Base.connection.execute(sql)
-        puts record[0].inspect
+        data = run_sql("SELECT COUNT(total) as total_sales FROM game_purchases")
+        puts data.inspect
     end
 
     def self.get_total_sales_by(column, value)
-        sql = "SELECT game_id, SUM(total) as total FROM game_purchases WHERE #{column} = #{value}"
-        record = ActiveRecord::Base.connection.execute(sql)
-        puts record.inspect
+        data = run_sql("SELECT game_id, SUM(total) as total FROM game_purchases WHERE #{column} = #{value}")
+        puts data.inspect
     end
 
     def self.get_total_sales_between(min, man)
-        sql = "SELECT game_id, SUM(total) as total FROM game_purchases WHERE total < #{max} AND total > #{min}"
-        record = ActiveRecord::Base.connection.execute(sql)
-        puts record.inspect
+        data = run_sql("SELECT game_id, SUM(total) as total FROM game_purchases WHERE total < #{max} AND total > #{min}")
+        puts data.inspect
     end
 
      def self.get_total_sales_between_dates(min_date, max_date)
-        sql = "SELECT game_id, SUM(total) as total FROM game_purchases WHERE created_at < #{max_date} AND created_at > #{min_date}"
-        record = ActiveRecord::Base.connection.execute(sql)
-        puts record.inspect
+        data = run_sql("SELECT game_id, SUM(total) as total FROM game_purchases WHERE created_at < #{max_date} AND created_at > #{min_date}")
+        puts data.inspect
     end
+
+    private 
+    def run_sql(sql)
+        return ActiveRecord::Base.connection.execute(sql)
+        # formather
+    end
+
 end
