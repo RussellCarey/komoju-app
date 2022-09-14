fcc0cfa0848dfb2a0ddfecf1b86ab94d61cf79c0
RussellCarey <62222824+RussellCarey@users.noreply.github.com>
2022-09-14 18:36:23 +0900

    Heroku test

--- Gemfile
+++ Gemfile
@@ -57,3 +57,6 @@ group :development do
   # Speed up commands on slow machines / big apps [https://github.com/rails/spring]
   # gem "spring"
 end
+
+gem 'rails_12factor'
+gem 'foreman'
--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,13 +7,13 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:8080"
+    origins "http://localhost:8080", "www.russell-carey.com"
 
     resource "*",
-      headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head],
-      # Exposes the auth token header?
-      expose: %w[Authorization Uid],
-      credentials: true
+             headers: :any,
+             methods: %i[get post put patch delete options head],
+             # Exposes the auth token header?
+             expose: %w[Authorization Uid],
+             credentials: true
   end
 end
